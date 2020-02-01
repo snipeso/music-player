b1b11cfff66b89a31a3fc28992ce51d357d9be39
snipeso <code@sophia.science>
2020-02-01 20:50:02 +0100

    new folder saving structure

--- mainMusic.py
+++ mainMusic.py
@@ -31,8 +31,7 @@ trigger = Trigger(CONF["trigger"]["serial_device"],
 screen = Screen(CONF)
 
 datalog = Datalog(OUTPUT_FOLDER=os.path.join(
-    'output', CONF["participant"] + "_" + CONF["session"],
-    datetime.datetime.now().strftime("%Y-%m-%d")), CONF=CONF)  # This is for saving data
+    'output', CONF["participant"] + "_" + CONF["session"]), CONF=CONF)  # This is for saving data
 
 kb = keyboard.Keyboard()
 
